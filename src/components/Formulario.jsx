56ad14df1f7c7e4162ceb645ca9a9d2f51793672
xandro2021 <xandrocastro2021@gmail.com>
2023-11-05 18:32:56 -0600

    Crear el formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { Button, Form, Row, Col, Alert } from 'react-bootstrap';
+import useCategorias from '../hooks/useCategorias';
+import { generarId } from '../helpers';
+
+const Formulario = () => {
+
+  const [busqueda, setBusqueda] = useState({
+    nombre: '',
+    categoria: ''
+  });
+
+  const [alerta, setAlerta] = useState('');
+  const { categorias } = useCategorias();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (Object.values(busqueda).includes('')) {
+      setAlerta('Todos los campos son obligatorios');
+      return;
+    }
+
+    setAlerta('');
+  };
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+    >
+      {alerta ?
+        <Alert variant="danger" className="text-center">{alerta}</Alert> : null
+      }
+
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="nombre">Nombre de la Bebida</Form.Label>
+
+            <Form.Control
+              id="nombre"
+              type="text"
+              placeholder="Ej: Tequila, Vodka, etc"
+              name="nombre"
+              value={busqueda.nombre}
+              onChange={e => setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value
+              })}
+            />
+          </Form.Group>
+        </Col>
+
+        <Col md={6}>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="categoria">Categoría de la Bebida</Form.Label>
+
+            <Form.Select
+              id="categoria"
+              name="categoria"
+              value={busqueda.categoria}
+              onChange={e => setBusqueda({
+                ...busqueda,
+                [e.target.name]: e.target.value
+              })}
+            >
+              <option value="">-- Selecciona Categoría --</option>
+
+              {categorias.map(categoria => (
+                <option key={generarId()} value={categoria.strCategory}>{categoria.strCategory}</option>
+              ))}
+            </Form.Select>
+
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-end">
+        <Col md={3}>
+          <Button
+            variant="danger"
+            className="text-uppercase w-100"
+            type="submit"
+          >
+            Buscador Bebidas
+          </Button>
+        </Col>
+      </Row>
+
+    </Form>
+  );
+};
+
+export default Formulario;
