3ff848febd81866880d57a182398b0187ea4b13a
xandro2021 <xandrocastro2021@gmail.com>
2023-11-06 17:25:37 -0600

    Mostrar los resultados de la consulta de la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import Formulario from './components/Formulario';
+import ListadoBebidas from './components/ListadoBebidas';
 import { CategoriasProvider } from './context/CategoriasProvider';
 import { BebidasProvider } from './context/BebidasProvider';
 
@@ -8,13 +9,16 @@ function App() {
   return (
     <CategoriasProvider>
       <BebidasProvider>
+
         <header className="py-5">
           <h1>Buscador de Bebidas</h1>
         </header>
 
         <Container className='mt-5'>
           <Formulario />
+          <ListadoBebidas />
         </Container>
+
       </BebidasProvider>
     </CategoriasProvider>
   )
---
+++ src/components/Bebida.jsx
@@ -0,0 +1,27 @@
+import { Col, Card, Button } from 'react-bootstrap';
+
+const Bebida = ({ bebida }) => {
+  return (
+    <Col md={6} lg={3}>
+      <Card className="mb-4">
+        <Card.Img
+          variant='top'
+          src={bebida?.strDrinkThumb}
+          alt={`Imagen de ${bebida?.strDrink}`}
+        />
+
+        <Card.Body>
+          <Card.Title>{bebida.strDrink}</Card.Title>
+          <Button
+            variant="warning"
+            className="w-100 text-uppercase mt-2"
+          >
+            Ver Receta
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default Bebida;
